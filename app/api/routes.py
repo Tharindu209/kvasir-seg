dc6c6ccbde7055b4cdb94cc74411c14edd7cbd53
Tharindu209 <tharindudamruwan23@gmail.com>
2025-02-03 23:10:42 +0530

    add new router

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
 from app.api.endpoints.auth import router as auth_router
+from app.api.endpoints.image import router as image_router
 
 routers = APIRouter()
-router_list = [auth_router]
+router_list = [auth_router, image_router]
 
 for router in router_list:
     routers.include_router(router)
