69ebf935b58a107e83f614df455878e9b8369388
Tharindu209 <tharindudamruwan23@gmail.com>
2025-02-03 23:11:55 +0530

    modify teh usermodel

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -6,11 +6,9 @@ class UserAuth(BaseModel):
     password: str = Field(..., min_length=5, max_length=24, description="user password")
     username: str = Field(..., description="user username")
     
-
 class UserOut(BaseModel):
     id: UUID
     email: str
 
-
 class SystemUser(UserOut):
-    password: str
+    pass
