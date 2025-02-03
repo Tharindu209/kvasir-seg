719e42328878b8caf0bb1b63d175e99dcfe10d80
Tharindu209 <tharindudamruwan23@gmail.com>
2025-02-03 23:11:23 +0530

    modify the configs

--- app/core/config.py
+++ app/core/config.py
@@ -3,16 +3,24 @@ from typing import List
 
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
+from supabase import create_client, Client
 
-load_dotenv()
+load_dotenv(verbose=True)
 
 ENV: str = ""
 
 class Configs(BaseSettings):
-    # base
     ENV: str = os.getenv("ENV", "dev")
     API: str = "/api"
-    PROJECT_NAME: str = "fca-api"
+    PROJECT_NAME: str = "Image-api"
+    # model_path: str = "models/best_model.pth"
+    allowed_file_types: list = ["image/jpeg", "image/png", "application/dicom"]
+    supabase_url: str = os.getenv("SUPABASE_URL")
+    supabase_key: str = os.getenv("SUPABASE_KEY")
+    JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY")
+    JWT_REFRESH_SECRET_KEY: str = os.getenv("JWT_REFRESH_SECRET_KEY")
+    ORIGINAL_IMAGES_BUCKET: str = "original_images"
+    SEGMENTED_IMAGES_BUCKET: str = "segment_images"
 
     PROJECT_ROOT: str = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -24,7 +32,9 @@ class Configs(BaseSettings):
     BACKEND_CORS_ORIGINS: List[str] = ["*"]
 
     class Config:
+        env_file = ".env"
         case_sensitive = True
 
 
 configs = Configs()
+supabase: Client = create_client(configs.supabase_url, configs.supabase_key)
