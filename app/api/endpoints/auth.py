4fcc75979982738f9ef6fb5d951998de69a4a81f
Tharindu209 <tharindudamruwan23@gmail.com>
2025-02-03 23:11:00 +0530

    change the auth for the supabase

--- app/api/endpoints/auth.py
+++ app/api/endpoints/auth.py
@@ -1,56 +1,59 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
-from fastapi.responses import RedirectResponse
 from app.schemas.user import UserAuth, UserOut, SystemUser
-from app.schemas.token import  TokenSchema
-from app.utils.auth_utils import create_access_token,create_refresh_token
-from app.utils.hash import encrypt_password,decrypt_password_and_verify
+from app.schemas.token import TokenSchema
+from app.utils.auth_utils import create_access_token, create_refresh_token
+from app.core.config import supabase
 from app.core.deps import get_current_user
-from uuid import uuid4
 
 router = APIRouter(
     prefix="/auth",
-    tags=["auth"],
+    tags=["Auth"],
 )
 
 @router.post('/signup', summary="Create new user", response_model=UserOut)
 async def create_user(data: UserAuth):
-    # querying database to check if user already exist
-    user = db.get(data.email, None)
-    if user is not None:
-            raise HTTPException(
+    try:
+        response = supabase.auth.sign_up({
+            "email": data.email,
+            "password": data.password,
+        })
+        user = response.user
+        return {"email": user.email, "id": user.id}
+    except Exception as e:
+        raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="User with this email already exist"
+            detail=str(e),
         )
-    user = {
-        'email': data.email,
-        'password': encrypt_password(data.password),
-        'id': str(uuid4())
-    }
-    db[data.email] = user    # saving user to database
-    return user
-
 
 @router.post('/login', summary="Create access and refresh tokens for user", response_model=TokenSchema)
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = db.get(form_data.username, None)
-    if user is None:
+    try:
+        response = supabase.auth.sign_in_with_password({
+            "email": form_data.username,
+            "password": form_data.password,
+        })
+        user = response.user
+        access_token = create_access_token(user.email, form_data.password)
+        refresh_token = create_refresh_token(user.email, form_data.password)
+        return { "access_token": access_token, "refresh_token": refresh_token}
+    except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Incorrect email or password"
+            detail="Incorrect email or password",
         )
-
-    hashed_pass = user['password']
-    if not decrypt_password_and_verify(form_data.password, hashed_pass):
+        
+@router.post('/logout', summary="Logout user")
+async def logout(user: SystemUser = Depends(get_current_user)):
+    try:
+        print(user.email)
+        response = supabase.auth.sign_out()
+        return {"message": "User logged out"}
+    except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Incorrect email or password"
+            detail=str(e),
         )
-    
-    return {
-        "access_token": create_access_token(user['email']),
-        "refresh_token": create_refresh_token(user['email']),
-    }
 
 @router.get('/me', summary='Get details of currently logged in user', response_model=UserOut)
 async def get_me(user: SystemUser = Depends(get_current_user)):
