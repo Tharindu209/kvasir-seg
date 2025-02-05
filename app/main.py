4d465ce7a6e2277df2fbad977c57b0d6d10ef7fa
Tharindu209 <tharindudamruwan23@gmail.com>
2025-02-05 21:48:54 +0530

    add the load_segmentation_model

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from starlette.middleware.cors import CORSMiddleware
 
 from app.api.routes import routers
 from app.core.config import configs
+from app.core.model_loader import load_segmentation_model
 
 app = FastAPI()
 
@@ -18,5 +19,9 @@ if configs.BACKEND_CORS_ORIGINS:
 @app.get('/')
 def read_root():
     return {'message': 'welcome to the API for KVASIR segmentation prediction'} 
+
+@app.on_event("startup")
+async def startup_event():
+    load_segmentation_model()
 
 app.include_router(routers)
