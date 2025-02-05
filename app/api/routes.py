2e8ed43e7ae77f1a752901b0e53185d6375a49ae
Tharindu209 <tharindudamruwan23@gmail.com>
2025-02-05 21:49:26 +0530

    add segmentation endpoint

--- app/api/routes.py
+++ app/api/routes.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from app.api.endpoints.auth import router as auth_router
 from app.api.endpoints.image import router as image_router
+from app.api.endpoints.segmentation import router as segmentation_router
+
 
 routers = APIRouter()
-router_list = [auth_router, image_router]
+router_list = [auth_router, image_router, segmentation_router]
 
 for router in router_list:
     routers.include_router(router)
