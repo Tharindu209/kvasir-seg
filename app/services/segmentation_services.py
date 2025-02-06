11db3407667924541c6087feef293555b82dae07
Tharindu209 <tharindudamruwan23@gmail.com>
2025-02-06 15:03:26 +0530

    fix the segmentation services

--- app/services/segmentation_services.py
+++ app/services/segmentation_services.py
@@ -5,7 +5,6 @@ import torch
 from io import BytesIO
 from app.core.config import configs, supabase
 from app.utils.image_processing import preprocess_image, postprocess_mask
-from app.core.model_loader import model
 
 class SegmentationService:
     def __init__(self, model):
@@ -13,14 +12,13 @@ class SegmentationService:
         self.device = torch.device(configs.DEVICE)
         self.model.to(self.device)
 
-    async def process_image(self, file_bytes: bytes, user_id: str) -> dict:
+    async def process_image(self, file_bytes: bytes, user_id: str, filename: str) -> dict:
         start_time = time.time()
-        
         try:
             image = preprocess_image(file_bytes)
             mask = self._run_inference(image)
-            result_url = await self._store_results(mask, user_id)
-            
+            print(mask)
+            result_url = await self._store_results(mask, user_id, filename)    
             return {
                 "segmented_url": result_url,
                 "processing_time": time.time() - start_time
@@ -36,20 +34,21 @@ class SegmentationService:
         with torch.no_grad():
             image_tensor = torch.tensor(image).permute(2, 0, 1).unsqueeze(0).to(self.device)
             output = self.model(image_tensor)
-            return postprocess_mask(output)
+            
+            preprocess_mask_image = postprocess_mask(output)
+            return preprocess_mask_image
 
-    async def _store_results(self, mask: np.ndarray, user_id: str) -> str:
+    async def _store_results(self, mask: np.ndarray, user_id: str, file_name: str) -> str:
         try:
             _, encoded_mask = cv2.imencode('.png', mask)
-            file_path = f"user_{user_id}/{int(time.time())}_segmented.png"
+            file_path = f"{configs.SEGMENTED_IMAGES_PATH}/{file_name}"
             
-            supabase.storage.from_(configs.SEGMENTATION_BUCKET).upload(
-                file_path=file_path,
+            res = supabase.storage.from_(configs.MAIN_BUCKET).upload(
                 file=encoded_mask.tobytes(),
-                file_options={"content-type": "image/png"}
+                path=file_path,
+                file_options={"cache-control": "3600", "upsert": "true", "content_type": "image/jpeg"},
             )
-            
-            return supabase.storage.from_(configs.SEGMENTATION_BUCKET).get_public_url(file_path)
+            return supabase.storage.from_(configs.MAIN_BUCKET).get_public_url(file_path)
             
         except Exception as upload_error:
             raise RuntimeError(f"Failed to upload result: {str(upload_error)}")
