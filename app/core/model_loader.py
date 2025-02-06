93e8add98b495008ab7e124934f4e5749eddc6cd
Tharindu209 <tharindudamruwan23@gmail.com>
2025-02-06 15:02:35 +0530

    update the model loader

--- app/core/model_loader.py
+++ app/core/model_loader.py
@@ -2,16 +2,20 @@ import torch
 import segmentation_models_pytorch as smp
 from app.core.config import configs
 
+model = None
+
 def load_segmentation_model():
     """Load and configure the segmentation model"""
-    model = smp.Unet(
+    global model
+    local_model = smp.Unet(
         encoder_name='resnet50',
         encoder_weights='imagenet',
         in_channels=3,
         classes=1
     )
-    model.load_state_dict(torch.load(configs.MODEL_PATH, map_location=torch.device(configs.DEVICE)))
-    model.eval()
+    
+    local_model.load_state_dict(torch.load(configs.MODEL_PATH, map_location=torch.device(configs.DEVICE)))
+    local_model.eval()
+    
+    model = local_model
     return model
-
-model = load_segmentation_model()
