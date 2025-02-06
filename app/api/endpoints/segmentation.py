57c8f677520345f80f68c9133413bd6872bfb9d8
Tharindu209 <tharindudamruwan23@gmail.com>
2025-02-06 15:01:47 +0530

    add the ImageServices

--- app/api/endpoints/segmentation.py
+++ app/api/endpoints/segmentation.py
@@ -1,24 +1,29 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from app.services.segmentation_services import SegmentationService
 from app.core.deps import get_current_user
+from app.core.config import configs
+from app.core.model_loader import model, load_segmentation_model
 from app.schemas.user import SystemUser
 from app.schemas.segmentation import SegmentationResult
+from app.services.image_services import ImageService
+
 
 router = APIRouter(
     prefix="/segmentation",
     tags=["Image Segmentation"],
 )
 
-@router.post("/process/{image_id}", response_model=SegmentationResult)
-async def process_image(
-    image_id: str,
-    current_user: SystemUser = Depends(get_current_user)
-    ):
-    
+model = load_segmentation_model()
+
+@router.post("/process/{image_name}", response_model=SegmentationResult)
+async def process_image(image_name: str,current_user: SystemUser = Depends(get_current_user) ):
     try:
-        result = await segmentation_service.process_image(image_id, current_user)
+        segmentation_service = SegmentationService(model)
+        image_path  = f'{configs.ORIGINAL_IMAGES_PATH}/{image_name}'
+        image = ImageService.get_the_image(image_path, configs.MAIN_BUCKET)
+        result = await segmentation_service.process_image(image, current_user.id, image_name)
         return {
-            "original_url": result["original_url"],
+            "original_url": ImageService.get_image_url(image_path, configs.MAIN_BUCKET),
             "segmented_url": result["segmented_url"],
             "processing_time": result["processing_time"]
         }
@@ -27,4 +32,4 @@ async def process_image(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=str(e)
         )
-        
+
